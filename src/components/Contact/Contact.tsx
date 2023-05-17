11b7f6e1923ea8857e38efab689172e7e500d8d6
gwollmann04 <gwollmann04@gmail.com>
2023-05-17 19:09:48 -0300

    Add some style contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,13 @@
-import { Flex, Text, Icon, useToast } from '@chakra-ui/react'
+import {
+  Flex,
+  Text,
+  useToast,
+  keyframes,
+  usePrefersReducedMotion,
+  Icon
+} from '@chakra-ui/react'
 import { useForm } from 'react-hook-form'
-import { BsEmojiSmile } from 'react-icons/bs'
+import { BsMailbox } from 'react-icons/bs'
 
 import { HeaderMenuSocialIcons, ContactForm } from '@/src/components'
 import { SOCIAL_MEDIA_OPTIONS } from '@/src/constants/menu'
@@ -8,10 +15,25 @@ import { contactResolver } from '@/src/services/contactYupResolvers'
 import { ContactFormType } from '@/src/@types/contact'
 import { sendEmail } from '@/src/services/contactServices'
 
+const textColorGradient = keyframes`
+  from {
+    background-position: 0% 50%;
+}
+  to {
+    background-position: 100% 50%;
+}
+`
+
 // Deixar mais bonito
-// automcomplete deixando branco
+// Remover commit das envs
 const Contact = () => {
   const toast = useToast()
+  const prefersReducedMotion = usePrefersReducedMotion()
+
+  const textColorAnimation = prefersReducedMotion
+    ? undefined
+    : `${textColorGradient} 3s ease-in-out infinite alternate`
+
   const {
     control,
     formState: { isSubmitting, errors },
@@ -50,7 +72,7 @@ const Contact = () => {
       py="84px"
       id="contact"
       as="form"
-      autoComplete="none"
+      autoComplete="off"
       onSubmit={handleSubmit(onSubmit)}
     >
       <ContactForm
@@ -58,19 +80,29 @@ const Contact = () => {
         errors={errors}
         control={control}
       />
-      <Flex direction="column" textAlign="center" fontSize="24px">
-        <Text justifyContent="center" display="flex">
+      <Flex direction="column" fontSize="24px">
+        <Text
+          display="flex"
+          sx={{
+            background: 'linear-gradient(to right, #6355a4, #6355a4, #e89a3e, #e89a3e)',
+            '-webkit-background-clip': 'text',
+            '-webkit-text-fill-color': 'transparent',
+            fontSize: '64px',
+            animation: textColorAnimation,
+            '-webkit-animation':  textColorAnimation,
+            backgroundSize: '300%',
+            '-moz-animation': textColorAnimation,
+            alignSelf:"end"
+          }}
+        >
           Lets get in touch!
-          <Icon
-            as={BsEmojiSmile}
-            alignSelf="center"
-            color="#E2E8F0"
-            boxSize="18px"
-            ml="12px"
-          />
         </Text>
-        <Text my="12px">Send me an e-mail</Text>
-        <Text>Or contact me in one of my socials</Text>
+        <Text display='flex'  my="36px">
+          Send me an e-mail... <Icon boxSize='32px' ml='12px' alignItems='center'  as={BsMailbox}/>
+        </Text>
+        <Text px="36px" mb="24px">
+          ...Or contact me in one of my socials
+        </Text>
         <Flex justifyContent="center" mt="12px">
           {SOCIAL_MEDIA_OPTIONS.map((option) => (
             <HeaderMenuSocialIcons key={option.value} option={option} />
